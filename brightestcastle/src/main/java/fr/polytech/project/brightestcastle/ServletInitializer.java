37a21f69ce7b715f9baba214aa583f53fa979a11
Anh-Kagi <anh.kagi@protonmail.com>
2021-11-16 09:16:54 +0100

    spring boot init
    from start.spring.io

---
+++ brightestcastle/src/main/java/fr/polytech/project/brightestcastle/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.polytech.project.brightestcastle;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BrightestCastleApplication.class);
+	}
+
+}
